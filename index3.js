24fde9219d3221a33e3e4bb05479721292b3f7a2
Tresina Fernando <126407464+TresinaFernandoCSE@users.noreply.github.com>
2023-03-30 21:52:54 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,42 @@
+var myname=("Nandhini"); 
+var collegename=prompt("Sri eshwar college of engineering"); 
+alert("My name is" + myname + "Studing at"+collegename);  
+prompt("Arithmetic operations");
+var addition=(2+3); 
+var subtraction=(5-4); 
+var division=(10/2); 
+var modulus=(14%7); 
+var multiplication=(2*8); 
+//increment and decrement  
+var x=2; 
+x++; 
+var y=6; 
+y--; 
+//Example problem for arithmetic operations - Dog age to human age converter 
+var dogage=2; 
+var humanage=(dogage-2)*4+21; 
+// conditional statements 
+let work = "Today";
+if (work === "Today") {
+  alert("Awesome you are great");
+} else {
+  alert("you are lazy");
+} 
+//while loop statements
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+} 
+//for loop 
+let i = 0;
+for (; i < 9; i++) {
+  console.log(i);
+  
+} 
+//for loop example program
+let j = 1;
+for (; j < 10; j += 2) {
+  console.log(j);
+}
